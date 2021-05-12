b8e261804b0c923fbc4c01a95c46608a2dad97e1
webpro2021 <kirill.smal2020@gmail.com>
2021-05-12 10:18:35 +0300

    store is modified

--- src/store/index.js
+++ src/store/index.js
@@ -13,7 +13,10 @@ export default new Vuex.Store({
     bnbBalance: 0.0,
     alertMsg: '',
     alert: false,
-    walletStatus: 'Unlock Wallet',
+    walletStatus: 'UNLOCK WALLET',
+    swapStatus: 'UNLOCK WALLET',
+    poolStatus: 'UNLOCK WALLET',
+    farmStatus: 'UNLOCK WALLET',
     viewStatus: 'Unlock View On FMT'
   },
 
@@ -24,6 +27,9 @@ export default new Vuex.Store({
     alertMsg: state => state.alertMsg,
     alert: state => state.alert,
     walletStatus: state => state.walletStatus,
+    swapStatus: state => state.swapStatus,
+    poolStatus: state => state.poolStatus,
+    farmStatus: state => state.farmStatus,
     viewStatus: state => state.viewStatus
   },
 
@@ -32,14 +38,21 @@ export default new Vuex.Store({
       if (appState.web3)
         state.web3 = appState.web3
 
+        console.log(appState, state)
       if (appState.walletAddress) {
         state.walletAddress = appState.walletAddress
-        if (appState.walletAddress !== 'Connect') {
+        if (appState.walletAddress !== 'CONNECT') {
           state.walletStatus = 'WALLET'
           state.viewStatus = 'VIEW ON FMTSCAN'
+          state.swapStatus = 'SWAP'
+          state.poolStatus = 'ADD LIQUIDITY'
+          state.farmStatus = 'FARMING'
         } else {
           state.walletStatus = 'UNLOCK WALLET'
           state.viewStatus = 'UNLOCK VIEW ON FMTSCAN'
+          state.swapStatus = 'UNLOCK WALLET'
+          state.poolStatus = 'UNLOCK WALLET'
+          state.farmStatus = 'UNLOCK WALLET'
         }
       }
 
@@ -68,6 +81,7 @@ export default new Vuex.Store({
 
         state.web3.eth.getBalance(state.walletAddress).then((res) => {
           state.bnbBalance = res / 1000000000000000000 //decimal is 18
+          
         }).catch(() => {
           state.bnbBalance = 0
         })
